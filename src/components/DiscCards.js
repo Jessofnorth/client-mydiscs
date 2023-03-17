35d38a3ed032779e265e648081fb9d5fb8301994
Jessica Ejelöv <jessicaejelov@gmail.com>
2023-03-17 19:38:11 +0100

    disc cards with dynamic data,  filtering buttons with dynamic data

--- src/components/DiscCards.js
+++ src/components/DiscCards.js
@@ -2,25 +2,44 @@ import React from "react";
 import { AiFillCheckCircle, AiFillCloseCircle } from "react-icons/ai";
 
 // component for displaying each disc and its info
-const DiscCards = () => {
+const DiscCards = (disc) => {
+  console.log(disc)
+
   return (
     <article className="card p-4 bg-sky-900/20 max-w-fit max-h-fit m-4 shadow-md rounded-lg">
       <img
         className="rounded-full border-8 border-white shadow-xl"
-        src="https://jess-mydiscs.azurewebsites.net/uploads/thumb_6wWtQrhc47A4sk3LhG2b231314693.jpg"
-        alt="Disc: namn"
+        src={disc.disc.imageName}
+        alt={"Disc: disc.name"}
       />
       <div className="text-gray-700 pt-4 text-lg grid grid-cols-2 content-center text-center gap-2">
-        <p className="text-2xl font-bold col-span-2">Eagle </p>
-        <p className="text-lg font-bold col-span-2">Innova - Fairway</p>
-        <p className="text-md col-span-2">Plastic: Star</p>
-        <p>Speed: <span className="font-bold">5</span></p>
-        <p>Glide: <span className="font-bold">2</span></p>
-        <p>Turn: <span className="font-bold">-3</span></p>
-        <p>Fade: <span className="font-bold">0</span></p>
-        <p className="text-xl col-span-2 text-green-800 flex place-self-center"><AiFillCheckCircle size={30} className="pr-2" />Disc is in bag</p>
-        <p className="text-xl col-span-2 text-red-800 flex place-self-center"><AiFillCloseCircle size={30} className="pr-2" />Disc is not in bag</p>
-
+        <p className="text-2xl font-bold col-span-2">{disc.disc.name}</p>
+        <p className="text-lg font-bold col-span-2">{disc.disc.brand.brandName} - {disc.disc.category.categoryName}</p>
+        <p className="text-md col-span-2">Plastic: {disc.disc.plastic}</p>
+        <p>
+          Speed: <span className="font-bold">{disc.disc.speed}</span>
+        </p>
+        <p>
+          Glide: <span className="font-bold">{disc.disc.glide}</span>
+        </p>
+        <p>
+          Turn: <span className="font-bold">{disc.disc.turn}</span>
+        </p>
+        <p>
+          Fade: <span className="font-bold">{disc.disc.fade}</span>
+        </p>
+        {/* show if disc is in bag or not based on bool from db */}
+        {disc.disc.bagged === true ? (
+          <p className="text-xl col-span-2 text-green-800 flex place-self-center">
+            <AiFillCheckCircle size={30} className="pr-2" />
+            Disc is in bag
+          </p>
+        ) : (
+          <p className="text-xl col-span-2 text-red-800 flex place-self-center">
+            <AiFillCloseCircle size={30} className="pr-2" />
+            Disc is not in bag
+          </p>
+        )}
       </div>
     </article>
   );
--- src/components/Discs.js
+++ src/components/Discs.js
@@ -12,90 +12,123 @@ const Discs = () => {
 
   // get the API calls and save to state
   useEffect(() => {
-   //   fetch discs from API
-  const fetchDiscs = async () => {
-    setLoading(true);
-    const getdiscs = await axios
-      .get(`https://jess-mydiscs.azurewebsites.net/api/disc`)
-      .then(function (response) {
-        if (response.status !== 200) {
-          throw new Error(
-            `This is an HTTP error: The status is ${response.status}`
-          );
-        }
-        setDiscs(response.data);
-        setError(null);
-        setLoading(false);
-      })
-      .catch(function (error) {
-        setDiscs([]);
-        setError(error.message);
-      });
-  };
-   //   fetch categories from API
-   const fetchCategories = async () => {
-    setLoading(true);
-    const getcategories = await axios
-      .get(`https://jess-mydiscs.azurewebsites.net/api/category`)
-      .then(function (response) {
-        if (response.status !== 200) {
-          throw new Error(
-            `This is an HTTP error: The status is ${response.status}`
-          );
-        }
-        setCategories(response.data);
-        setError(null);
-        setLoading(false);
-      })
-      .catch(function (error) {
-        setCategories([]);
-        setError(error.message);
-      });
-  };
-  //   // get brands from API
+    //   fetch discs from API
+    const fetchDiscs = async () => {
+      setLoading(true);
+      const getdiscs = await axios
+        .get(`https://jess-mydiscs.azurewebsites.net/api/disc`)
+        .then(function (response) {
+          if (response.status !== 200) {
+            throw new Error(
+              `This is an HTTP error: The status is ${response.status}`
+            );
+          }
+          setDiscs(response.data);
+          setError(null);
+          setLoading(false);
+        })
+        .catch(function (error) {
+          setDiscs([]);
+          setError(error.message);
+        });
+    };
+    //   fetch categories from API
+    const fetchCategories = async () => {
+      setLoading(true);
+      const getcategories = await axios
+        .get(`https://jess-mydiscs.azurewebsites.net/api/category`)
+        .then(function (response) {
+          if (response.status !== 200) {
+            throw new Error(
+              `This is an HTTP error: The status is ${response.status}`
+            );
+          }
+          setCategories(response.data);
+          setError(null);
+          setLoading(false);
+        })
+        .catch(function (error) {
+          setCategories([]);
+          setError(error.message);
+        });
+    };
+    //   // get brands from API
 
-  const fetchBrands = async () => {
-    setLoading(true);
-    const getbrands = await axios
-      .get(`https://jess-mydiscs.azurewebsites.net/api/brand`)
-      .then(function (response) {
-        if (response.status !== 200) {
-          throw new Error(
-            `This is an HTTP error: The status is ${response.status}`
-          );
-        }
-        setBrands(response.data);
-        setError(null);
-        setLoading(false);
-      })
-      .catch(function (error) {
-        setBrands([]);
-        setError(error.message);
-      });
-  };
-  fetchBrands();
-  fetchCategories();
-  fetchDiscs();
+    const fetchBrands = async () => {
+      setLoading(true);
+      const getbrands = await axios
+        .get(`https://jess-mydiscs.azurewebsites.net/api/brand`)
+        .then(function (response) {
+          if (response.status !== 200) {
+            throw new Error(
+              `This is an HTTP error: The status is ${response.status}`
+            );
+          }
+          setBrands(response.data);
+          setError(null);
+          setLoading(false);
+        })
+        .catch(function (error) {
+          setBrands([]);
+          setError(error.message);
+        });
+    };
+    fetchBrands();
+    fetchCategories();
+    fetchDiscs();
   }, []);
-  
 
   return (
     <main className="flex flex-col min-h-screen">
-      <nav className="grid md:grid-cols-3 m-4">
-        <p>Test 1</p>
-        <p>Test 2</p>
-        <p>Test 3</p>
-      </nav>
+      <div className="grid md:grid-cols-3 m-4 font-bold">
+        {/* navigations with filter options */}
+        <nav>
+          <h2 className="text-md font-bold text-center py-3">Brand</h2>
+          <div className="flex justify-evenly flex-wrap">
+            {brands.map((brand) => (
+              <button
+                key={brand.brandId}
+                className="border-black border-2 shadow-md m-1 text-xs hover:bg-black hover:text-white"
+              >
+                {brand.brandName}
+              </button>
+            ))}
+          </div>
+        </nav>
+        <nav>
+          <h2 className="text-md font-bold text-center py-3">In bag</h2>
+          <div className="flex justify-center flex-wrap">
+            <button className="border-black border-2 shadow-md m-1 text-xs hover:bg-black hover:text-white">
+              Yes
+            </button>
+            <button className="border-black border-2 shadow-md m-1 text-xs hover:bg-black hover:text-white">
+              No
+            </button>
+          </div>
+        </nav>
+        <nav>
+          <h2 className="text-md font-bold text-center py-3">Disctype</h2>
+          <div className="flex justify-evenly flex-wrap">
+            {categories.map((category) => (
+                <button
+                  key={category.categoryId}
+                    className="border-black border-2 shadow-md m-1 text-xs hover:bg-black hover:text-white"
+                >
+                    {category.categoryName}
+                </button>
+            ))}
+          </div>
+        </nav>
+      </div>
       <section className="flex flex-row flex-wrap justify-evenly ">
         {/* show error or loading when needed */}
-        {loading && <div>Loading discs...</div>}
+        {loading && <div>Loading...</div>}
         {error && (
           <div className="text-red-600">{`There is a problem fetching the discs - ${error}`}</div>
         )}
-        <DiscCards />
-        <DiscCards />
-        <DiscCards />
-        <DiscCards />
+  {discs.map((disc) => (
+          <DiscCards key={disc.discId} disc={disc} />   
+        ))}
       </section>
     </main>
   );
