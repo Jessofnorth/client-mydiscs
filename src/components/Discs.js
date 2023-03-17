509b18124d660576f12274568ea07e46b98b7809
Jessica Ejelöv <jessicaejelov@gmail.com>
2023-03-17 15:44:47 +0100

    install axios, fetch discs. brands and categories and save to state

--- src/components/Discs.js
+++ src/components/Discs.js
@@ -1,32 +1,83 @@
 import React, { useState, useEffect } from "react";
 import DiscCards from "./DiscCards";
+import axios from "axios";
 
 // component for the main disc grid
 const Discs = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [discs, setDiscs] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [brands, setBrands] = useState([]);
 
+  // get the API calls and save to state
   useEffect(() => {
-    fetch(`https://jess-mydiscs.azurewebsites.net/api/disc`)
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error(`This is an HTTP error: The status is ${res.status}`);
+   //   fetch discs from API
+  const fetchDiscs = async () => {
+    setLoading(true);
+    const getdiscs = await axios
+      .get(`https://jess-mydiscs.azurewebsites.net/api/disc`)
+      .then(function (response) {
+        if (response.status !== 200) {
+          throw new Error(
+            `This is an HTTP error: The status is ${response.status}`
+          );
         }
-        return res.json();
-      })
-      .then((data) => {
-        setDiscs(data);
+        setDiscs(response.data);
         setError(null);
-      })
-      .catch((e) => {
-        setDiscs([]);
-        setError(e.message);
-      })
-      .finally(() => {
         setLoading(false);
+      })
+      .catch(function (error) {
+        setDiscs([]);
+        setError(error.message);
       });
+  };
+   //   fetch categories from API
+   const fetchCategories = async () => {
+    setLoading(true);
+    const getcategories = await axios
+      .get(`https://jess-mydiscs.azurewebsites.net/api/category`)
+      .then(function (response) {
+        if (response.status !== 200) {
+          throw new Error(
+            `This is an HTTP error: The status is ${response.status}`
+          );
+        }
+        setCategories(response.data);
+        setError(null);
+        setLoading(false);
+      })
+      .catch(function (error) {
+        setCategories([]);
+        setError(error.message);
+      });
+  };
+  //   // get brands from API
+
+  const fetchBrands = async () => {
+    setLoading(true);
+    const getbrands = await axios
+      .get(`https://jess-mydiscs.azurewebsites.net/api/brand`)
+      .then(function (response) {
+        if (response.status !== 200) {
+          throw new Error(
+            `This is an HTTP error: The status is ${response.status}`
+          );
+        }
+        setBrands(response.data);
+        setError(null);
+        setLoading(false);
+      })
+      .catch(function (error) {
+        setBrands([]);
+        setError(error.message);
+      });
+  };
+  fetchBrands();
+  fetchCategories();
+  fetchDiscs();
   }, []);
+  
 
   return (
     <main className="flex flex-col min-h-screen">
